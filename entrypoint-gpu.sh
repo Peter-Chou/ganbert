c132bcd28b350fcbf45d6ab47618bbb198703336
Peter-Chou <zhou_00009@126.com>
2020-10-09 14:41:01 +0800

    :lipstick:

--- entrypoint-gpu.sh
+++ entrypoint-gpu.sh
@@ -6,7 +6,7 @@ touch /logs/weibo_filter.err.log && touch /logs/weibo_filter.out.log
 
 nohup tensorflow_model_server --port=8500 --rest_api_port=8501 \
       --model_name=ganbert --model_base_path=/saved_model \
-      --per_process_gpu_memory_fraction=0.2 \
+      --per_process_gpu_memory_fraction=$GPU_MEMORY_FRACTION \
       --enable_model_warmup=true > tf_serving.log 2>&1 &
 
 # python gateway.py
